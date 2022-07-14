3113399bd8ddb812c88387c710a25fd9151ae4e5
Dhruvsurani <dhruvsurani2000@gmail.com>
2022-07-15 00:42:14 +0530

    added some changes

--- topic_pre.py
+++ topic_pre.py
@@ -1,10 +1,14 @@
 from google.cloud import pubsub_v1
+import os
 
+from google.pubsub_v1.types import pubsub
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/dhruvsurani/Downloads/miconcept-355405-88c28fcf47f4.json"
 GOOGLE_PROJECT_ID = 'miconcept-355405'
-TOPIC = 'dhruvsurani2000'
-
+TOPIC = 'projects/miconcept-355405/models/census'
+# https://pubsub.googleapis.com/v1/{name}
 publisher = pubsub_v1.PublisherClient()
-# topic_path = f"projects/{GOOGLE_PROJECT_ID}/topics/{TOPIC}"
-topic_path = publisher.topic_path(GOOGLE_PROJECT_ID, TOPIC)
+topic_path = f"projects/{GOOGLE_PROJECT_ID}/topics/{TOPIC}"
+# topic_path = publisher.topic_path(GOOGLE_PROJECT_ID, TOPIC)
 
 publisher.create_topic(topic_path)
